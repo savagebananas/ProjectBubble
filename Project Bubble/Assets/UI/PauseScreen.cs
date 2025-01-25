4e04127e9b86c45794ef2f4e8873bf8e11c554b5
Ciyu-Chen <135166473+Ciyu-Chen@users.noreply.github.com>
2025-01-25 11:48:07 -0600

    Pause Screen and Game Over

---
+++ Project Bubble/Assets/UI/PauseScreen.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+	public GameObject pauseScreen;
+    public GameObject gameOverScreen;
+	public static bool isPaused;
+    public playerOxygen Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseScreen.SetActive(false);
+        gameOverScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+        if(Player.oxygen <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+    	pauseScreen.SetActive(true);
+    	Time.timeScale = 0f; //pauses time in game
+    	isPaused = true;
+    }
+
+    public void MainMenu()
+    {
+    	Time.timeScale = 1f;
+    	SceneManager.LoadScene("StartScene");
+    }
+
+    public void RetryGame()
+    {
+    	Scene currentScene = SceneManager.GetActiveScene();
+    	SceneManager.LoadScene(currentScene.name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+}
