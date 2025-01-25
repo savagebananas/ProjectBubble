f066887079ee3dcb0bbfa1f98bbcb9ddd3a28dc3
Ciyu-Chen <135166473+Ciyu-Chen@users.noreply.github.com>
2025-01-25 16:18:53 -0600

    playerShooting

---
+++ Project Bubble/Assets/Fishing/BubbleGun.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BubbleGun : MonoBehaviour
+{
+	public GameObject bubbleBulletPrefab;
+	public Transform firePoint;
+	public float fireForce = 20.0f;
+
+	public void Fire()
+	{
+		GameObject bubbleBullet = Instantiate(bubbleBulletPrefab, firePoint.position, firePoint.rotation);
+		bubbleBullet.GetComponent<Rigidbody>().AddForce(firePoint.right * fireForce, ForceMode.Impulse);
+
+	}
+}
---
+++ Project Bubble/Assets/Fishing/Fishing.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Fishing : MonoBehaviour
+{
+	public Rigidbody rb;
+	public BubbleGun bubbleGun;
+
+	Vector3 mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+        	bubbleGun.Fire();
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+        	mousePosition = hit.point;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+    	Vector3 aimDirection = (mousePosition - rb.position).normalized;
+    	float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+    	rb.rotation = Quaternion.Euler(0, 0, aimAngle);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+    	
+    }
+}
